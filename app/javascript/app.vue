ee7c785c61eff6c71a0c930ee609bdfd91336b78
d-khorolsuren <d-khorolsuren@fullout.jp>
2020-06-30 20:11:11 +0900

    first commit

---
+++ app/javascript/app.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <Header/>
+    <dashboard/>
+  </div>
+</template>
+
+<script>
+
+  import Header from './components/header.vue'
+  import dashboard from './components/Dashboard.vue'
+  
+export default {
+  name: 'app',
+  components: {
+    Header,
+    dashboard
+  },
+  data: function () {
+  },
+}
+</script>
